bd34694443efaac33b798af92d9c987007910632
Dan Ryan <hi@iamdanryan.com>
2011-04-07 23:53:11 -0400

    bump

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-# Create default metrics
-
-[ "Load Averages", "Disk Usage", "Memory Usage", "Swap Usage" ].each do |metric|
-  Metric.find_or_create_by(:name => metric)
-end
--- features/support/mongoid.rb
+++
@@ -1,3 +0,0 @@
-Mongoid.master.collections.select do |collection|
-  collection.name !~ /system/
-end.each(&:drop)
