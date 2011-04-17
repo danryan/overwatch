1931354c2d21d951230439d3e0f01012f5824be4
Dan Ryan <hi@iamdanryan.com>
2011-04-17 19:31:53 -0400

    don't need ohm helpers anymore

--- lib/overwatch/helpers.rb
+++ lib/overwatch/helpers.rb
@@ -1,14 +1,4 @@
 module Overwatch
   module Helpers
-    def format_errors(model)
-      error_messages = model.errors.present do |e|
-        # Missing attributes
-        e.on [:name, :not_present], "Name attribute missing"
-        e.on [:data, :not_present], "Data attribute missing"
-        # Uniqueness
-        e.on [:name, :not_unique], "Name already taken"
-      end
-      error_messages.first
-    end
   end
 end
